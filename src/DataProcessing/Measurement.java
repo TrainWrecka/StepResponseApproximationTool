4ff8e8471fabca120b5ec6a1aab96d608b180bdd
TrainWrecka <tomsi.frei@gmail.com>
2017-05-03 18:19:04 +0200

    Added filtering
    Filtering of signal with filtfilt working

--- src/DataProcessing/Measurement.java
+++ src/DataProcessing/Measurement.java
@@ -1,12 +1,12 @@
 package DataProcessing;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.jfree.data.xy.XYSeries;
 
-import com.sun.org.apache.xerces.internal.util.Status;
-
+import matlabfunctions.Filtfilt;
 import userinterface.StatusBar;
 
 public class Measurement {
@@ -22,6 +22,7 @@ public class Measurement {
 	private PlotData plotData = new PlotData();
 
 	private boolean input = false;
+	int stepLocation = 0;
 
 	public Measurement() {
 
@@ -33,6 +34,17 @@ public class Measurement {
 		measurementData = convertList(measurementList);
 
 		extractData();
+
+		if (input) {
+		//	getStepLocation();
+		//	getOffset();
+		}
+		
+		if(checkNoise() == true){
+			filtFunction();
+		}
+		
+
 		if (checkNoise() == false) {
 			//cutData2();
 		}
@@ -42,15 +54,15 @@ public class Measurement {
 		plotData.removeStepresponseData();
 		//plotData.setStepresponseData(cutData);
 		//plotData.setStepresponseData(measurementData);
-		if(input){
+		if (input) {
 			plotData.setStepresponseData(timeData, inputData, stepData);
 		} else {
 			plotData.setStepresponseData(timeData, stepData);
 		}
-		
+
 		StatusBar.showStatus("Number of points: " + timeData.length);
-		StatusBar.showStatus("Last entry: " + timeData[timeData.length-1]);
-		
+		StatusBar.showStatus("Last entry: " + timeData[timeData.length - 1]);
+
 	}
 
 	public double[][] getMeasurement() {
@@ -158,49 +170,143 @@ public class Measurement {
 		}
 	}
 
+	/*
+	 * if three columns are found, the format of the data is [time, input, output]
+	 * if two columns are found, the format of the data is [time, output]
+	 * else an error message is printed
+	 * if an input column exist a flag gets set
+	 */
 	private void extractData() {
 		input = false;
-		
-		int lengthrow = measurementData.length;
-		int lengthcolumn = measurementData[0].length;
-		
-		
 
-		switch (measurementData[0].length) {
-			case 3:
-				timeData = new double[measurementData.length];
-				inputData = new double[measurementData.length];
-				stepData = new double[measurementData.length];
-				for (int i = 0; i < measurementData.length; i++) {
-					timeData[i] = measurementData[i][0];
-					inputData[i] = measurementData[i][1];
-					stepData[i] = measurementData[i][2];
-				}
-				input = true;
-				break;
+		if (measurementData[0].length == 3) {
+			timeData = new double[measurementData.length];
+			inputData = new double[measurementData.length];
+			stepData = new double[measurementData.length];
+			for (int i = 0; i < measurementData.length; i++) {
+				timeData[i] = measurementData[i][0];
+				inputData[i] = measurementData[i][1];
+				stepData[i] = measurementData[i][2];
+			}
+			input = true;
+		} else if (measurementData[0].length == 2) {
+			timeData = new double[measurementData.length];
+			inputData = new double[1];
+			stepData = new double[measurementData.length];
+			for (int i = 0; i < measurementData.length; i++) {
+				timeData[i] = measurementData[i][0];
+				stepData[i] = measurementData[i][1];
+			}
+		} else if (measurementData[0].length == 0) {
+			StatusBar.showStatus("No data found");
+		} else {
+			StatusBar.showStatus("Too many data columns");
+		}
+	}
 
-			case 2:
-				timeData = new double[measurementData.length];
-				inputData = new double[1];
-				stepData = new double[measurementData.length];
-				for (int i = 0; i < measurementData.length; i++) {
-					timeData[i] = measurementData[i][0];
-					stepData[i] = measurementData[i][1];
-				}
+	private void getStepLocation() {
+		stepLocation = 0;
+		for (int i = 0; i < inputData.length; i++) {
+			if (inputData[i] != 0) {
+				stepLocation = i - 1;
+				StatusBar.showStatus("Step location:" + Integer.toString(i));
 				break;
+			}
+		}
+	}
 
-			case 0:
-				StatusBar.showStatus("No data found");
-				break;
+	private void getOffset() {
+		double[] tempArray = Arrays.copyOfRange(stepData, 0, stepLocation);
+		double offset = mean(tempArray);
+		StatusBar.showStatus("Offset:" + Double.toString(offset));
+		removeOffset(offset);
+		//double offset = mean(y(1:cut1));
+	}
 
-			default:
-				StatusBar.showStatus("Too many data columns");
-				break;
+	private void removeOffset(double offset) {
+		for (int i = 0; i < stepData.length; i++) {
+			stepData[i] -= offset;
 		}
 	}
 
 	public boolean inputExisting() {
 		return input;
+	}
+	
+	
+	/*
+	 * hoffentli chomi do i einere woche no druus...
+	 */
+	private void filtFunction() {
+		//ArrayList<Double> stepDataList = Arrays.asList(stepData);
+		//List<Double> list = new ArrayList<Double>(Arrays.asList(stepData));
+
+		int iN = 30;
+		double[] yFiltered = null;
+		double[] ySignal = null;
+		
+		ArrayList<Double> oneOne; 
+		ArrayList<Double> onesArrayList;
+		ArrayList<Double> stepDataList = new ArrayList<Double>();
+		for(double d : stepData) stepDataList.add(d);
+		
+		//Double[] doubleArray = ArrayUtils.toObject(stepData);
+		//ArrayList<Double> stepDataList = Arrays.asList(doubleArray);
+		
+		double noiseError = 1;
+		ArrayList<Double> yFilteredList;
+
+		while (Math.abs(noiseError) > 1.9e-3) {
+			Double[] onesArray = new Double[iN];
+			
+
+			oneOne = new ArrayList<Double>();
+			oneOne.add(1.0);
+			
+			for (int i = 0; i < onesArray.length; i++) {
+				onesArray[i] = 1.0 / iN;
+				
+			}
+			
+			onesArrayList =  new ArrayList<Double>();
+			for(double d : onesArray) onesArrayList.add(d);
+
+			
+			
+			yFilteredList = Filtfilt.doFiltfilt(onesArrayList, oneOne, stepDataList);
+
+			yFiltered = new double[yFilteredList.size()];
+			
+			ySignal = new double[yFilteredList.size()];
+			
+			
+
+			for (int i = 0; i < yFilteredList.size(); i++) {
+				ySignal[i] = yFilteredList.get(i);
+				yFiltered[i] = (stepData[i] - yFilteredList.get(i)) / stepData[i];
+			}
+
+			noiseError = mean(yFiltered);
+
+			iN--;
+			
+			if(iN == 0){
+				StatusBar.showStatus("iN = 0");
+				break;
+			}
+		}
+		
+		inputData = ySignal;
+		
+
+	}
+
+	private static double mean(double[] m) {
+		double sum = 0;
+		for (int i = 0; i < m.length; i++) {
+			sum += m[i];
+		}
+		return sum / m.length;
 	}
 
 }
